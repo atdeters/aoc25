e00d9eb875eccba8b96b248eeaa067cdb9747f20
N-Determine <sreted@outlook.com>
2025-12-02 03:17:25 +0100

    feat: Add -std=c++98

--- day01/run-pt1.sh
+++ day01/run-pt1.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-g++ -D PART=1 src/day01.cpp -o day01pt1
+g++ -D PART=1 src/day01.cpp -std=c++98 -o day01pt1
 ./day01pt1 input/input.txt
 rm day01pt1
--- day01/run-pt2.sh
+++ day01/run-pt2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-g++ -D PART=2 src/day01.cpp -o day01pt2
+g++ -D PART=2 src/day01.cpp -std=c++98 -o day01pt2
 ./day01pt2 input/input.txt
 rm day01pt2
