d800ce3cde7485aebac06ff8ec19e33eacdc36d4
atdeters <sreted@outlook.com>
2025-12-03 17:54:16 +0100

    chore: Remove empty helper files

--- day03/run-pt2.sh
+++ day03/run-pt2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd src
-cc main.c helpers.c -D DIGITS=12 -o day03 --std=c99 -Wall -Werror -Wextra
+cc main.c -D DIGITS=12 -o day03 --std=c99 -Wall -Werror -Wextra
 ./day03 ../input/input.txt
 rm day03
